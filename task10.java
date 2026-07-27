20cf83005b165a90b7dc2b3ade09ec84993b0e74
s-saimohithreddy <vtu29732@veltech.edu.in>
2026-07-27 11:22:45 +0530

    Implement sum of last digits of two numbers

---
+++ task10.java
@@ -0,0 +1,6 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+last_digit1 = num1 % 10
+last_digit2 = num2 % 10
+sum_last_digits = last_digit1 + last_digit2
+print("Sum of last digits =", sum_last_digits)
