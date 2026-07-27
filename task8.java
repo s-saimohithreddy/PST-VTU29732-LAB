d6a3f7f044b9ca99627e4fe73a58781305de19b1
s-saimohithreddy <vtu29732@veltech.edu.in>
2026-07-27 11:23:33 +0530

    Fix Fibonacci function and remove user input

--- task8.java
+++ task8.java
@@ -1,4 +1,3 @@
-# Function to find the nth Fibonacci number
 def fibonacci(n):
     if n <= 0:
         return "Enter a positive integer"
@@ -16,9 +15,3 @@ def fibonacci(n):
         b = c
 
     return b
-
-# Input from user
-n = int(input("Enter the value of n: "))
-
-# Output
-print("The", n, "th Fibonacci number is:", fibonacci(n))
